b1769bb092e419eef12af91e009f59abb36d3894
Paulina <pauwic@proton.me>
2024-04-27 10:09:08 +0200

    losowe numery i suma

--- game.py
+++ game.py
@@ -1,5 +1,8 @@
-# %% 
-print('Hello')
+import random
 
-print('Bartosz')
-# %%
+
+random_number1 = random.randint(1,6)
+random_number2 = random.randint(1,6)
+random_sum = random_number1 + random_number2
+print(f"Wylosowałeś: {random_number1} i {random_number2}")
+print(f"Twój wynik: {random_sum}")
