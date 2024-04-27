4e0880d96ddca511edfa0b7dee0682883f7448ee
Toricode00 <wiktoria.noganska@gmail.com>
2024-04-27 10:23:36 +0200

    powtarzanie losowania i zatrzymanie na 1

--- game.py
+++ game.py
@@ -1,8 +1,10 @@
 import random
-
-
-random_number1 = random.randint(1,6)
-random_number2 = random.randint(1,6)
-random_sum = random_number1 + random_number2
-print(f"Wylosowałeś: {random_number1} i {random_number2}")
-print(f"Twój wynik: {random_sum}")
+random_number1 = 0
+random_number2 = 0
+   
+while random_number1 !=1 and random_number2 !=1 :
+    random_number1 = random.randint(1,6)
+    random_number2 = random.randint(1,6)
+    random_sum = random_number1 + random_number2
+    print(f"Wylosowałeś: {random_number1} i {random_number2}")
+    print(f"Twój wynik: {random_sum}")
